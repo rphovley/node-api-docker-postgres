42f8fd0881715a98de08df383850dd07a45267cf
Paul Hovley <rphovley@gmail.com>
2020-05-07 12:15:12 -0600

    updated app template

--- server/src/utils/firebase_config.ts
+++ server/src/utils/firebase_config.ts
@@ -6,7 +6,7 @@ import admin from 'firebase-admin'
 
 const initializeFirebase = (): void => {
   const serviceAccount = require('../env/google_service_accounts/firebase_service_account.json')
-  if (!serviceAccount) throw Error('Missing firebase configs')
+  if (!serviceAccount) throw Error('Missing firebase config')
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
